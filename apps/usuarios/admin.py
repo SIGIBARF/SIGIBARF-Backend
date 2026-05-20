132a631d37c0bfb848a3e4807fc60844e33d3754
SebastianPenarandaNieto <sebastianpeni@ufps.edu.co>
2026-05-20 16:08:25 -0500

    ADD: Registro de los modelos en admin

--- apps/usuarios/admin.py
+++ apps/usuarios/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.usuarios.models import Rol, Usuario
+
+
+@admin.register(Rol)
+class RolAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    search_fields = ('nombre',)
+
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'apellido', 'correo', 'rol', 'telefono')
+    list_filter = ('rol',)
+    search_fields = ('nombre', 'apellido', 'correo', 'telefono')
+    autocomplete_fields = ('rol',)
