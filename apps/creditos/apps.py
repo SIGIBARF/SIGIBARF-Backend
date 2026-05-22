5adb92a31b2ca64f84c713945cce7da598b5487c
RogerUFPS <rogersantiagomh@ufps.edu.co>
2026-05-21 22:02:25 -0500

    Fix 1.4

--- apps/creditos/apps.py
+++ apps/creditos/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class CreditosConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.creditos"
-    label = "accounts"
+    label = "creditos"
--- project/settings.py
+++ project/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
     "apps.usuarios",
-    "apps.creditos",
     "apps.ventas",
     "apps.inventario",
     "apps.creditos",
